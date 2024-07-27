1d1f47674ebcfb3e71d143fdd326ea1d4d69d131
deshan-ravin <135998785+deshan-ravin@users.noreply.github.com>
2024-07-27 15:56:28 +0530

    config changes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
     images: {
       domains: ['firebasestorage.googleapis.com'], // Allow images from Firebase Storage
     },
+
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
   };
   
   export default nextConfig;
